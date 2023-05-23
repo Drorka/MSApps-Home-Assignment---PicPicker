177666250d0d625a5e274233156151a2cdf6cba5
Drorka~ <drorpa4@gmail.com>
2023-05-23 18:47:50 +0300

    remove unnecessary functions

--- frontend/src/store/photo.actions.js
+++ frontend/src/store/photo.actions.js
@@ -1,14 +1,10 @@
-import { pixabayService } from '../services/pixabay.service'
-// import { photoService } from '../services/photo.service.local.js'
 import { photoService } from '../services/photo.service.js'
 import { store } from '../store/store.js'
 import { SET_PHOTOS } from './photo.reducer.js'
 
-export async function loadPhotos() {
+export async function loadPhotos(filterBy) {
 	try {
-		// const photos = await pixabayService.query('random', 2)
-		const category = { categoryName: 'random' }
-		const photos = await photoService.query(category)
+		const photos = await photoService.query(filterBy)
 		console.log('Photos from DB:', photos)
 		store.dispatch({
 			type: SET_PHOTOS,
@@ -18,12 +14,4 @@ export async function loadPhotos() {
 		console.log('Cannot load photos', err)
 		throw err
 	}
-}
-
-export async function prevPhotos() {
-	console.log('herro action prev')
-}
-
-export async function nextPhotos() {
-	console.log('herro action next')
 }
