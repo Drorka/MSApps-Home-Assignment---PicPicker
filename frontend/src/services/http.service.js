b2f1ebb5aba5d4a15850223fe87bebbaf6539611
Drorka~ <drorpa4@gmail.com>
2023-05-27 18:49:47 +0300

    optimize services

--- frontend/src/services/http.service.js
+++ frontend/src/services/http.service.js
@@ -1,46 +1,24 @@
 import Axios from 'axios'
 
-const BASE_URL =
-	process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/'
+export const httpService = {
+	get,
+}
+const BASE_URL = '//localhost:3030/api/'
 
-var axios = Axios.create({
+const axios = Axios.create({
+	baseURL: BASE_URL,
 	withCredentials: false,
 })
 
-export const httpService = {
-	get(endpoint, data) {
-		return ajax(endpoint, 'GET', data)
-	},
-	post(endpoint, data) {
-		return ajax(endpoint, 'POST', data)
-	},
-	put(endpoint, data) {
-		return ajax(endpoint, 'PUT', data)
-	},
-	delete(endpoint, data) {
-		return ajax(endpoint, 'DELETE', data)
-	},
-}
-
-async function ajax(endpoint, method = 'GET', data = null) {
+async function get(endpoint, data) {
 	try {
-		const res = await axios({
-			url: `${BASE_URL}${endpoint}`,
-			method,
-			data,
-			params: method === 'GET' ? data : null,
-		})
+		const res = await axios(endpoint, { params: data })
 		return res.data
-	} catch (err) {
+	} catch (error) {
 		console.error(
-			`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: `,
+			`Had Issues getting to the backend, endpoint: ${endpoint}, with data: `,
 			data
 		)
-		console.dir(err)
-		if (err.response && err.response.status === 401) {
-			sessionStorage.clear()
-			window.location.assign('/')
-		}
-		throw err
+		throw error
 	}
 }
--- frontend/src/services/photo.service.js
+++ frontend/src/services/photo.service.js
@@ -1,17 +1,14 @@
 import { httpService } from './http.service.js'
 
-const STORAGE_KEY = 'photos'
-
 export const photoService = {
 	query,
 }
-window.cs = photoService
 
 async function query(searchCriteria) {
 	try {
-		return await httpService.get(STORAGE_KEY, searchCriteria)
-	} catch (err) {
-		console.error('Failed to get photos (service)')
-		throw err
+		return await httpService.get('photos', searchCriteria)
+	} catch (error) {
+		console.error('Failed to get photos')
+		throw error
 	}
 }
