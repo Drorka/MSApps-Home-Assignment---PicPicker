67f7d562dceb7dd19bf6fb3df70eb842183c89c7
Drorka~ <drorpa4@gmail.com>
2023-05-27 13:06:43 +0300

    add category button arrow

--- frontend/src/components/photos-actions.jsx
+++ frontend/src/components/photos-actions.jsx
@@ -6,13 +6,21 @@ import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded'
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 
 export function PhotosActions({
 	onPageChange,
 	onToggleModal,
 	onSort,
 	categoryBtn,
+	isCategoryModalOpen,
 }) {
+	const categoryButtonDirection = isCategoryModalOpen ? (
+		<ExpandLessIcon />
+	) : (
+		<ExpandMoreIcon />
+	)
 	return (
 		<section className="photos-actions">
 			<Stack spacing={2} direction="row">
@@ -27,6 +35,7 @@ export function PhotosActions({
 					onClick={() => onToggleModal('category')}
 					ref={categoryBtn}
 					variant="contained"
+					endIcon={categoryButtonDirection}
 				>
 					Categories
 				</Button>
