f878bc349c7e444255983e7893438582523cdd92
Drorka~ <drorpa4@gmail.com>
2023-05-27 18:06:17 +0300

    add key to list element

--- frontend/src/components/photo-list.jsx
+++ frontend/src/components/photo-list.jsx
@@ -5,7 +5,7 @@ export function PhotoList({ photos, onPhoto }) {
 	return (
 		<ul className="photo-list clean-list">
 			{photos.map((photo) => (
-				<Grow in={true} timeout={500} appear={true}>
+				<Grow in={true} timeout={500} appear={true} key={photo.id}>
 					<li
 						onClick={() => onPhoto(photo.id)}
 						className="photo-preview"
