c8bc2ff2b5ac86836c60fa4de878f9dcba30fc5d
Drorka~ <drorpa4@gmail.com>
2023-05-27 12:26:42 +0300

    update server structure

--- backend/server.js
+++ backend/server.js
@@ -4,36 +4,22 @@ const path = require('path')
 
 const app = express()
 const http = require('http').createServer(app)
-
-// Express App Config
-app.use(express.json())
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-	const corsOptions = {
-		origin: [
-			'http://127.0.0.1:8080',
-			'http://localhost:8080',
-			'http://127.0.0.1:3000',
-			'http://localhost:3000',
-		],
-		credentials: true,
-	}
-	app.use(cors(corsOptions))
-}
-
 const photoRoutes = require('./api/photo/photo.routes')
 
-// routes
-app.use('/api/photo', photoRoutes)
+app.use(express.json())
 
-// Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/photo/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-app.get('/**', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+const corsOptions = {
+	origin: [
+		'http://127.0.0.1:8080',
+		'http://localhost:8080',
+		'http://127.0.0.1:3000',
+		'http://localhost:3000',
+	],
+	credentials: true,
+}
+app.use(cors(corsOptions))
+
+app.use('/api/photo', photoRoutes)
 
 const port = process.env.PORT || 3030
 http.listen(port, () => {
